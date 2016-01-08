edd2902eb41f4a0ab42def941d5c80ea0d6f4e79
Tundish <tundish@thuswise.org>
2016-01-08 17:04:18 +0000

    Minor: notes.

--- maloja/workflow/path.py
+++ maloja/workflow/path.py
@@ -12,3 +12,8 @@ This structure contains the file path components necessary to
 identify resource stored as YAML data in the Maloja survey tree.
 
 """
+
+# TODO: Surveyor.patterns go here
+# TODO: Factories for empty Paths
+# TODO: Integrate with model classes
+# TODO: Needs a search API cf find_xpath

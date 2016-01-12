5eabfae9bcaa2ad19f7b0dcf311629fc56bc8761
tundish <tundish@thuswise.org>
2016-01-12 14:32:55 +0000

    Tidy.

--- maloja/workflow/path.py
+++ maloja/workflow/path.py
@@ -28,9 +28,10 @@ Path = namedtuple(
 )
 """
 This structure contains the file path components necessary to
-identify resource stored as YAML data in the Maloja survey tree.
+identify a resource stored as YAML data in the Maloja survey tree.
 
 """
+
 
 def cache(path, obj=None):
     parent = os.path.join(*(i for i in path[:-1] if i is not None))
@@ -46,6 +47,7 @@ def cache(path, obj=None):
         finally:
             locks[path].release()
     return fP
+
 
 def find_ypath(path: Path, query, **kwargs):
     """
@@ -80,51 +82,6 @@ def find_ypath(path: Path, query, **kwargs):
                     hit = Path(*fP.split(os.sep)[-len(path):])
                     yield (hit._replace(root=path.root), obj)
 
-# TODO: Surveyor.patterns go here
-# TODO: Factories for empty Paths
-# TODO: Integrate with model classes
-# TODO: Needs a search API cf find_xpath
-
-def filter_records(*args, root="", key="", value=""):
-    """
-    Reads files from the argument list, turns them into objects and yields
-    those which match key, value criteria.
-
-    Matching of attributes flattens the object hierarchy. Attributes at the top
-    level of the object take precedence over those with the same name further
-    down in its children.
-
-    Return values are 2-tuples of object and path.
-
-    """
-    for fP in args:
-        path = split_to_path(fP, root)
-        name = os.path.splitext(path.file)[0]
-        try:
-            typ, pattern = Surveyor.patterns[name]
-        except KeyError:
-            continue
-
-        with open(fP, 'r') as data:
-            obj = typ(**yaml_loads(data.read()))
-            if obj is None:
-                continue
-            if not key:
-                yield (obj, path)
-                continue
-            else:
-                data = dict([
-                    (k, getattr(item, k))
-                    for seq in [
-                        i for i in vars(obj).values() if isinstance(i, list)
-                    ]
-                    for item in seq
-                    for k in getattr(item, "_fields", [])],
-                    **vars(obj)
-                )
-                match = data.get(key.strip(), "")
-                if value.strip() in str(match):
-                    yield (obj, path)
 
 def split_to_path(data, root=None):
     drive, tail = os.path.splitdrive(data)

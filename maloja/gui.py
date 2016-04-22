b17a3a380047fc861496413f637e7157863d440a
tundish <tundish@thuswise.org>
2016-04-22 16:29:50 +0100

    First workiing dialog. Includes help button.

--- maloja/gui.py
+++ maloja/gui.py
@@ -1,5 +1,24 @@
+#!/usr/bin/env python
+#   -*- encoding: UTF-8 -*-
+
+# Copyright Skyscape Cloud Services
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import tkinter as tk
 from tkinter import messagebox
+from tkinter.font import Font
+import webbrowser
 
 def user_valid(text):
     return text
@@ -21,32 +40,47 @@ class ValidEntry(tk.Entry):
 
 class DialogFrame(tk.Frame):
 
-    INSTRUCTION_FONT = '20px'
+    """
+    Not for advanced use.
+    Please consult the documentation on how to operate
+    the Maloja command line interface (CLI).
+
+    """
 
     def __init__(self, root, *args, intro=None, **kwargs):
         super().__init__(root, *args, **kwargs)
+
+        h1 = Font(family="Verdana", size=10, weight="normal")
+        h2 = Font(family="Courier", size=12, weight="normal")
+        h3 = Font(family="Helvetica", size=10, weight="bold")
+
         if intro is None:
             intro = self.__doc__
         self.intro = tk.Label(
             root,
-            font=DialogFrame.INSTRUCTION_FONT,
+            font=h1,
             justify=tk.LEFT,
             text=intro
         )
-        self.intro.grid(row=0, sticky=tk.N)
+        self.intro.grid(row=0, column=0, columnspan=6)
 
-        tk.Label(root, text="URL:").grid(row=1, column=0, sticky=tk.W)
-        self.url = ValidEntry(root, validator=url_valid)
-        self.url.grid(row=1, column=1, columnspan=4, sticky=tk.E)
+        tk.Label(root, text="URL:", font=h2).grid(row=1, column=0, sticky=tk.E)
+        self.url = ValidEntry(root, width=80, validator=url_valid)
+        self.url.grid(row=1, column=1, columnspan=5)
 
-        tk.Label(root, text="User:").grid(row=2, column=0, sticky=tk.W)
-        self.user = ValidEntry(root, validator=user_valid)
-        self.user.grid(row=2, column=1, columnspan=4, sticky=tk.E)
+        tk.Label(root, text="User:", font=h2).grid(row=2, column=0, sticky=tk.E)
+        self.user = ValidEntry(root, width=80, validator=user_valid)
+        self.user.grid(row=2, column=1, columnspan=5)
 
-        self.survey = tk.Button(root, command=self.survey, text="Survey")
+        self.survey = tk.Button(root, command=self.survey, text="Survey", font=h3)
         self.survey.grid(row=3, column=1)
-        self.quit = tk.Button(root, text="Quit", command=root.destroy)
+        self.quit = tk.Button(root, text="Quit", command=root.destroy, font=h3)
         self.quit.grid(row=3, column=3)
+        self.help = tk.Button(root, text="Help...", command=self.help, font=h3)
+        self.help.grid(row=3, column=5)
+
+    def help(self):
+        webbrowser.open_new_tab("http://pythonhosted.org/maloja")
 
     def survey(self):
         try:
@@ -68,16 +102,14 @@ class MalojaGui(tk.Tk):
 
     def __init__(self, *args, title="GUI", **kwargs):
         super().__init__(*args, **kwargs)
-        self.grid(300, 120, 5, 5)
         self.resizable(False, False)
         self.title_label = tk.Label(self, font=self.TITLE_FONT, text=title)
-        # self.title_label.grid()
-        self.frame = DialogFrame(self)
-        self.frame.grid(row=0, column=0)
+        self.frame = DialogFrame(self, width=120, height=60)
+        self.frame.grid()
         self.wm_title(title)
         self.update()
         self.geometry(self.geometry())
 
 if __name__ == '__main__':
-    app = MalojaGui(title="Maloja quick ops")
+    app = MalojaGui(title="Maloja command panel")
     app.mainloop()
